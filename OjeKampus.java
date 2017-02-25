c658931a3ac4c1d9bdad826bea8c35faa8bc0be7
valdaoz19 <rizka.salsabila51@ui.ac.id>
2017-02-25 23:54:02 +0700

    modul 1

---
+++ OjeKampus.java
@@ -0,0 +1,30 @@
+
+/**
+ * Write a description of class OjeKampus here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class OjeKampus
+{
+    // instance variables - replace the example below with your own
+
+    /**
+     * Constructor for objects of class OjeKampus
+     */
+    public OjeKampus()
+    {
+        // initialise instance variables
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public void main(String[] args)
+    {
+        
+    }
+}
